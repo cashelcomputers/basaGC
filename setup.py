3bee2e03fd53520998814d66990d34641eb208f5
cashelcomputers <cashelcomputers@gmail.com>
2014-11-23 22:37:22 +1300

    Added requirements and basagc.maneuvers

--- setup.py
+++ setup.py
@@ -6,10 +6,11 @@ from distutils.core import setup
 setup(
     name='basaGC',
     version='0.5.3',
-    packages=['basagc'],
+    packages=['basagc', 'basagc.maneuvers'],
     url='https://github.com/cashelcomputers/basaGC',
     license='GPL',
     author='Tim Buchanan',
     author_email='cashelcomputers@gmail.com',
-    description='A implementation of the Apollo Guidance Computer for Kerbal Space Program'
+    description='A implementation of the Apollo Guidance Computer for Kerbal Space Program',
+    requires=['sortedcontainers']
 )
